ec29ce86a0974e99b25f328db043e5f79c902c70
the Phoeron <thephoeron@me.com>
2013-11-21 09:31:58 -0500

    Add custom REPL prompt

--- uber-shell.lisp
+++ uber-shell.lisp
@@ -2,9 +2,18 @@
 
 ;;; Top-level UBER-SHELL package
 (in-package #:uber-shell)
-
+;; customize prompt and install to REPL
+(defun custom-prompt ()
+  "If running SBCL and not linedit on darwin or unix/linux, installs the custom UBER-SHELL prompt to the REPL."
+  #+(and sbcl (or darwin unix) (not linedit))
+  (setf sb-int:*repl-prompt-fun*
+        (lambda (stream)
+          (format stream "~%~C[31m#[~A::~A]>~C[0m " #\Escape (lisp-implementation-type) (or (first (package-nicknames *package*))(package-name *package*)) #\Escape))))
+(custom-prompt)
+  
+;; default cmd function
 (defun cmd (name &rest rest)
-  "The default CMD macro"
+  "The default CMD function"
   #+(and sbcl (or darwin unix))
   (sb-ext:run-program name rest :search t :input t :output t))
 
